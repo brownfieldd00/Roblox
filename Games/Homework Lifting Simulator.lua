e26f9df5e6219206b9d1299628190184fef0a842
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-09 03:33:52 -0500

    Update Homework Lifting Simulator.lua

--- Games/Homework Lifting Simulator.lua
+++ Games/Homework Lifting Simulator.lua
@@ -1,6 +1,3 @@
 -- https://www.roblox.com/games/4840156698/Pets-Homework-Lifting-Simulator
 util = loadstring(game:HttpGet('https://raw.githubusercontent.com/brownfieldd00/unwrapper/main/source.lua'))()
-
-game:GetService('RunService').RenderStepped:Connect(function()
-    util:executeNow(function() game:GetService('Players').LocalPlayer.Character.Weights.E:FireServer() end)
-end)
+util:RS(function() util:executeNow(function() game:GetService('Players').LocalPlayer.Character.Weights.E:FireServer() end) end)
