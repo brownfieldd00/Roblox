de42d0ada98f24f728c44be86b4b3f7a5494d360
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-09 14:44:44 -0500

    Update RoStreets.lua

--- WIP/RoStreets.lua
+++ WIP/RoStreets.lua
@@ -1,3 +1,5 @@
+util = loadstring(game:HttpGet('https://raw.githubusercontent.com/brownfieldd00/unwrapper/main/source.lua'))()
+
 local Player = game:GetService("Players").LocalPlayer
 local Melee = Player.Backpack:FindFirstChild("Melee") or Player.Character:FindFirstChild("Melee")
 
@@ -14,14 +16,14 @@ end
 
 Melee.Parent = Player.Character 
 
-for Data,Model in pairs(game:GetService("Workspace")["Cash_Givers"]:GetChildren()) do 
+for Data, Model in pairs(game:GetService("Workspace")["Cash_Givers"]:GetChildren()) do 
     if Model:IsA("Model") and Model.Name == "Atm" and Model:FindFirstChild("Health") and not tostring(Model:FindFirstChild("Health").Value):match("-") then 
-       game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = Model.Open.CFrame + Vector3.new(0, 0, 5)
-       repeat task.wait() 
-          mouse1click()
-       until tostring(Model:FindFirstChild("Health").Value):match("-")
-       repeat task.wait()
-         CheckCash()
-       until not CheckCash() or not Player.Character or not Player.Character:FindFirstChild("HumanoidRootPart")
+        util:teleportToLocationPatchA(Model.Open.CFrame + Vector3.new(0, 0, 5))
+        repeat task.wait() 
+            mouse1click()
+        until tostring(Model:FindFirstChild("Health").Value):match("-")
+        repeat task.wait()
+            CheckCash()
+        until not CheckCash() or not Player.Character or not Player.Character:FindFirstChild("HumanoidRootPart")
     end
 end 
