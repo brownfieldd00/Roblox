065166688b4272e934e7ffd305639bd84dc3bd25
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-09 03:04:36 -0500

    add game link

--- Games/Homework Lifting Simulator.lua
+++ Games/Homework Lifting Simulator.lua
@@ -1,3 +1,4 @@
+-- https://www.roblox.com/games/4840156698/Pets-Homework-Lifting-Simulator
 game:GetService('RunService').RenderStepped:Connect(function()
     pcall(function()
        game.Players.LocalPlayer.Character.Weights.E:FireServer()
