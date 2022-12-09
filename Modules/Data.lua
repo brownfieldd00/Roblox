d97223b8528dfaacb02cea823ab923aae77b2e7c
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-09 05:02:53 -0500

    Create Data.lua

---
+++ Modules/Data.lua
@@ -0,0 +1,19 @@
+local Data = {}
+Data.cache = getgc(true)
+Data.tables = {}
+for i, v in pairs(Data.cache) do
+    if type(v) == 'table' then
+        table.insert(Data.tables, v)
+    end
+end
+
+function Data:deepSearch(key)
+    for _, module in pairs(Data.tables) do
+        if rawget(module, key) then
+            return module
+        end
+    end
+    return nil
+end
+
+return Data
