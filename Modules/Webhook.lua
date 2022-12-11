1e4011419c9cd5dfb7d4d7c3f3b68123f275698c
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-11 01:40:34 -0500

    Update Webhook.lua

--- Modules/Webhook.lua
+++ Modules/Webhook.lua
@@ -1,7 +1,9 @@
 Webhook = {}
+Webhook.__index = Webhook
 Webhook.HttpService = game:GetService("HttpService")
 function Webhook.new(url)
     local webhook = {}
+    webhook.__index = webhook
     webhook.url = url
     function webhook:send(content)
         return Webhook.HttpService:PostAsync(self.url, Webhook.HTTPService:JSONEncode({content = content}))
