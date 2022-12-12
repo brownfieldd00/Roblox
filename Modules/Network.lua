e5c3283817e4c60624e071581959a08df24b5f04
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-12 04:11:03 -0500

    Network

--- Modules/Network.lua
+++ Modules/Network.lua
@@ -7,4 +7,24 @@ end
 function Network:post(url, body)
     return self.HS:PostAsync(url, self.HS:JSONEncode(body))
 end
+Network.cachedInstances = getinstances()
+function Network:getRemoteEvent(name, parent, cache)
+    local cache = cache or true
+    local location
+    if cache == true then
+        location = self.cachedInstances
+    else
+        location = getinstances()
+    end
+    for i, v in pairs(location) do
+        if parent and v.Parent ~= parent then
+            continue
+        end
+        if v.Name == name then
+            return v
+        else
+            return nil
+        end
+    end
+end
 return Network
