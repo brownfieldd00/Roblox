d67c75b67a799a535fa4cd701e6ed14c075002e1
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-13 03:10:13 -0500

    Create Player.lua

---
+++ Modules/Player.lua
@@ -0,0 +1,58 @@
+Player = {}
+Player.LocalPlayer = game:GetService('Players').LocalPlayer
+Player.NC = game:GetService("NetworkClient")
+
+function Player:getLocalService(service_name)
+	return game:GetService(service_name)
+end
+function Player:getCharacter()
+	return self.LocalPlayer.Character or self.LocalPlayer.CharacterAdded:Wait()
+end
+function Player:getHumanoidRootPart()
+	return Player:getCharacter():FindFirstChild('HumanoidRootPart')
+end
+function Player:getHumanoid(max)
+	local max = max or 3
+	return self:getCharacter():WaitForChild('Humanoid', max)
+end
+function Player:setOutgoingKBPS(input_int)
+	return self.NC:SetOutgoingKBPSLimit(input_int)
+end
+function Player:getGuiWithName(input_string)
+	for i, v in pairs(self.LocalPlayer.PlayerGui:GetChildren()) do
+		if v.Name == input_string then
+			return v
+		end
+	end
+	return nil
+end
+function Player:getBackpackToolWithName(input_string)
+	for i, v in pairs(self.LocalPlayer.Backpack:GetChildren()) do
+		if v.Name == input_string then
+			return v
+		end
+	end
+	return nil
+end
+function Player:disableLocalScriptFromName(input_string)
+	for i, v in pairs(self.LocalPlayer.PlayerGui:GetChildren()) do
+		if v:IsA('Script') and v.Name == input_string then
+			v.Disabled = true
+			return v
+		end
+	end
+	return nil
+end
+function Player:disableLocalScript(input_script)
+	input_script.Disabled = true
+	return true
+end
+function Player:teleportToPosition(input_vector3)
+	self:getHumanoidRootPart().CFrame = CFrame.new(input_vector3)
+	return true
+end
+function Player:teleportToCFrame(input_cframe)
+	self:getHumanoidRootPart().CFrame = input_cframe
+	return true
+end
+return Player
