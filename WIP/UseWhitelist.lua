7a04f726870946be62ebb418e8b753cdf32bdbe0
David Brownfield <brownfieldd00@gmail.com>
2022-12-16 17:12:08 -0500

    whitelist key removal

--- WIP/UseWhitelist.lua
+++ WIP/UseWhitelist.lua
@@ -1,6 +1,6 @@
 core = loadstring(game:HttpGet('https://raw.githubusercontent.com/brownfieldd00/core/main/core.lua'))()
 Whitelist = core:gModule('Whitelist')
-Whitelist:setCode('hete7j1b-g0z3axvm-ysi7kvrt-r95la5ol-wrt3ny4r')
+Whitelist:setCode()
 Whitelist:get()
 synapseFingerprint = brown:identifySynapseUser()
 -- exclusive access to a brown library
