71621fb01ff484c4681fdaee32c2e86494f6c1be
David Brownfield <brownfieldd00@gmail.com>
2022-12-16 17:09:22 -0500

    Update UseWhitelist.lua

--- WIP/UseWhitelist.lua
+++ WIP/UseWhitelist.lua
@@ -4,4 +4,4 @@ Whitelist:setCode('hete7j1b-g0z3axvm-ysi7kvrt-r95la5ol-wrt3ny4r')
 Whitelist:get()
 synapseFingerprint = brown:identifySynapseUser()
 -- exclusive access to a brown library
-brown.doSomeShit()
+brown:OpenDiscord('Roblox')
