ce92921e0245bb1133cfcdae91153e522c9a3846
David Brownfield <brownfieldd00@gmail.com>
2022-12-16 06:31:46 -0500

    Update Whitelist.lua

--- Modules/Whitelist.lua
+++ Modules/Whitelist.lua
@@ -1,15 +1,18 @@
 local Whitelist = {}
+Whitelist.code = ''
 if not core then return {error('no thanks')} end
 --if not Synapse then return {error('no thanks')} end
 function Whitelist:get()
     local data = syn.request({
         Url = 'https://whitelist.brownfieldd00.workers.dev',
         Method = 'GET',
-        Headers = {
-            Key = 'hete7j1b-g0z3axvm-ysi7kvrt-r95la5ol-wrt3ny4r'
-        }
+        Headers = {Key = self.code}
     })
     return loadstring(data.Body)()
+end
+function Whitelist:setCode(newCode)
+    self.code = newCode
+    return true
 end
 return setmetatable(Whitelist, {
     __newindex = function()
