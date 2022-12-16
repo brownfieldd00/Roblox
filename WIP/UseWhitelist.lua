ddd607a75c515a3801ddec260858cea05c64085a
David Brownfield <brownfieldd00@gmail.com>
2022-12-16 06:04:39 -0500

    Update UseWhitelist.lua

--- WIP/UseWhitelist.lua
+++ WIP/UseWhitelist.lua
@@ -2,7 +2,5 @@ core = loadstring(game:HttpGet('https://raw.githubusercontent.com/brownfieldd00/
 -- Whitelist: ./Modules/Whitelist.lua
 Whitelist = core:gModule('Whitelist')
 Whitelist:get()
--- brown: C:\Users\brown\OneDrive\Documents\GitHub\brown-private-source\brown.lua
--- brown: C:\Users\brown\OneDrive\Documents\GitHub\brown-private-source\main.lua
-
 a = brown:identifySynapseUser()
+print(a)
