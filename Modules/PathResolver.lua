514db6b0ad98bf9ff8176ab0040bd4cdaab2a0d1
David Brownfield <brownfieldd00@gmail.com>
2022-12-18 23:04:43 -0500

    Create PathResolver.lua

---
+++ Modules/PathResolver.lua
@@ -0,0 +1,26 @@
+local PathResolver = {}
+PathResolver.__index = PathResolver
+
+function PathResolver:resolvePath(path)
+    local resolve = function(current_path, current_in_path)
+		if current_path == nil and current_in_path == 'game' then
+			return game
+		elseif current_path == game then
+			return game:GetService(current_in_path)
+		else
+			if not current_path:FindFirstChild(current_in_path) then
+				return nil
+			else
+				return current_path[current_in_path]
+			end
+		end
+	end
+	local current;
+	for _, inside_path in pairs(path:split('.')) do
+		current = resolve(current, inside_path)
+		if current == nil then
+			break
+		end
+	end
+    return current
+end
